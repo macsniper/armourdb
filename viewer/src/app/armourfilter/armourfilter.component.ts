c89da497bedc29c23c853a851088cee6a5962cf3
Clemens Mayer <macsniper@gmail.com>
2017-12-29 18:31:01 +0100

    refactoring

--- viewer/src/app/armourfilter/armourfilter.component.ts
+++ viewer/src/app/armourfilter/armourfilter.component.ts
@@ -34,20 +34,19 @@ export class ArmourfilterComponent implements OnInit {
 
   onSubmit() {
     let filterObject:any = {};
-    if(!(_.isEmpty(this.model.armourtype) || this.model.armourtype === "undefined")) {
-      filterObject.typeParsed = this.model.armourtype;
-    } 
-    if(!(_.isEmpty(this.model.productionCity) || this.model.productionCity === "undefined")) {
-      _.set(filterObject, ['basic data', 'attributed production city'], this.model.productionCity);
-    }
-    if(!(_.isEmpty(this.model.productionRegion) || this.model.productionRegion === "undefined")) {
-      _.set(filterObject, ['basic data', 'attributed production region'], this.model.productionRegion);
-    }
+    this.setFilterField(filterObject, ['typeParsed'], this.model.armourtype);
+    this.setFilterField(filterObject, ['basic data', 'attributed production city'], this.model.productionCity);
+    this.setFilterField(filterObject, ['basic data', 'attributed production region'], this.model.productionRegion);
     if(!_.isEmpty(this.model.freetext)) {
       this.armourService.setFreetext(this.model.freetext);
     }
-    debugger;
     this.armourService.setFilter(filterObject);
+  }
+
+  private setFilterField(filterObject, path, value) {
+    if(!(_.isEmpty(value) || value === "undefined")) {
+      _.set(filterObject, path, value);
+    }
   }
 }
  
